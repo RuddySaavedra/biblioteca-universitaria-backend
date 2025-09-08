1e680c648e1f7512cfd68012f843689b2e7b08c1
matilderuiz666 <a2024112013@estudiantes.upsa.edu.bo>
2025-09-07 21:56:15 -0400

    Inventory

---
+++ src/main/java/com/app/bibliotecauniversitariapa/dtos/InventoryDTO.java
@@ -0,0 +1,16 @@
+package com.app.grupo.dtos;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class InventoryDTO {
+    private Long id;
+    private int totalCopies;
+    private int availableCopies;
+    private int minThreshold;
+}

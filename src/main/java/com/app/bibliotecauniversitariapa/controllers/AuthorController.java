1d35bcad7df9bea4479a0046a30d8667d904d9b4
mariadelcarmen123 <a2024112587@estudiantes.upsa.edu.bo>
2025-09-16 13:07:30 -0400

    Controlador de la clase author completado

--- src/main/java/com/app/bibliotecauniversitariapa/controllers/AuthorController.java
+++ src/main/java/com/app/bibliotecauniversitariapa/controllers/AuthorController.java
@@ -1,10 +1,13 @@
 package com.app.bibliotecauniversitariapa.controllers;
 
+import com.app.bibliotecauniversitariapa.dtos.AuthorDTO;
 import com.app.bibliotecauniversitariapa.services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/authors")
@@ -12,4 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthorController {
     @Autowired
     private AuthorService authorService;
+
+
+    @PostMapping
+    public ResponseEntity<?> addAuthor(@RequestBody AuthorDTO authorDTO) {
+        AuthorDTO savedAuthorDTO = authorService.createAuthor(authorDTO);
+        return new ResponseEntity<>(savedAuthorDTO, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AuthorDTO>> getAllAuthors() {
+        List<AuthorDTO> authorDTOS = authorService.getAuthors();
+        return new ResponseEntity<>(authorDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AuthorDTO> getAuthorById(@PathVariable Long id) {
+        AuthorDTO authorDTO = authorService.getAuthorById(id);
+        return new ResponseEntity<>(authorDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<?> updateAuthor(@PathVariable Long id, @RequestBody AuthorDTO authorDTO) {
+        AuthorDTO updateAuthorDTO = authorService.updateAuthor(id, authorDTO);
+        return new ResponseEntity<>(updateAuthorDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteAuthor(@PathVariable Long id) {
+        authorService.deleteAuthor(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
